6b39af7549fd603b336113cf044b5d65fb52f474
radovanDj <radovandjurdjevic89@gmail.com>
2023-01-19 10:36:27 +0100

    dodata schema_extra u pydantic model

--- pydanticmodel.py
+++ pydanticmodel.py
@@ -5,8 +5,8 @@ from pydantic import BaseModel, Field
 # ovo je schemas.py fajl koji u FastAPI dokumentaciji postoji
 class Podatak(BaseModel):
     """ovo je klasa koja definise velikana i u sebi ima 
-    ime - ime coeka
-    prezime - prezime coeka
+    ime - ime čoveka
+    prezime - prezime čoveka
     """
     ime: str = Field(default="")
     prezime: str = Field(default="")
@@ -14,6 +14,12 @@ class Podatak(BaseModel):
     class Config:
         """ovo je za pydantic i sluzi da se prenesu neke informacije pydantic-u za mapiranje
         """
+        schema_extra = {
+            "example": {
+                "id": 2,
+                "ime": "Radovan",
+                "prezime": "Đurđević"}
+        }
         orm_mode = True
         allow_population_by_field_name = True
         arbitrary_types_allowed = False
